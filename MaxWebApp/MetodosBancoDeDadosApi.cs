06912336c3e555f3df5ee3af928c46433ff40238
Max Willian <max.willian@moveresoftware.com>
2024-06-17 14:51:49 -0400

    API

---
+++ MaxWebApp/MetodosBancoDeDadosApi.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using static MaxWebApp.Operacao;
+using System.Security.Policy;
+using System.Text;
+
+namespace MaxWebApp
+{
+	public class MetodosBancoDeDadosApi
+	{
+		public static async Task<List<Item>> CarregarItensDoInventarioGET(string url)
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				HttpResponseMessage response = await client.GetAsync(url);
+				response.EnsureSuccessStatusCode();
+				string responseBody = await response.Content.ReadAsStringAsync();
+				return JsonConvert.DeserializeObject<List<Item>>(responseBody);
+			}
+		}
+		public static async Task AdicionarItemPOST(string url, Item item)
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				var json = JsonConvert.SerializeObject(item);
+				var content = new StringContent(json, Encoding.UTF8, "application/json");
+				HttpResponseMessage responseMessage = await client.PostAsync(url, content);
+				responseMessage.EnsureSuccessStatusCode();
+			}
+		}
+		public static async Task AtualizarItemPUT(string url, Item item)
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				var json = JsonConvert.SerializeObject(item);
+				var content = new StringContent(json, Encoding.UTF8, "application/json");
+				HttpResponseMessage responseMessage = await client.PutAsync(url, content);
+				responseMessage.EnsureSuccessStatusCode();
+			}
+		}
+		public static async Task DeletarItemDELETE(string url)
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				HttpResponseMessage responseMessage = await client.DeleteAsync(url);
+				responseMessage.EnsureSuccessStatusCode();
+			}
+		}
+	}
+}
